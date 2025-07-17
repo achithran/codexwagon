ab7aca9a285c36c2cbff4fbf8d32df39ea8a5ba7
Achithran <achithran@gmail.com>
2025-07-17 15:26:37 +0530

    snap slide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Services from '../components/Services';
@@ -8,14 +9,28 @@ import CTA from '../components/CTA';
 
 export default function Home() {
   return (
-    <main className="bg-gray-50 min-h-screen font-sans">
-      <Hero />
-      <About />
-      <Services />
-      <AdvancedAI />
-      <WhyChoose />
-      <Testimonials />
-      <CTA />
+    <main className="h-screen w-full overflow-y-scroll scroll-smooth snap-y snap-mandatory">
+      <section className="snap-start">
+        <Hero />
+      </section>
+      <section className="snap-start">
+        <About />
+      </section>
+      <section className="snap-start">
+        <Services />
+      </section>
+      <section className="snap-start">
+        <AdvancedAI />
+      </section>
+      <section className="snap-start">
+        <WhyChoose />
+      </section>
+      <section className="snap-start">
+        <Testimonials />
+      </section>
+      <section className="snap-start">
+        <CTA />
+      </section>
     </main>
   );
 }
